3997a0aefedae8878d898c77ddf198218d3aded0
Oliver Gierke <info@olivergierke.de>
2015-10-15 12:19:44 +0200

    Restructured example.
    Simple mode without service discovery (i.e. the requirement for external resources) still runnable. Simply run the customer and store services using mvn spring-boot:run.
    
    The customer service now uses Hystrix to guard the resource discovery. The monitoring stream can be observed by running the hystrix-dashboard and pointing it to http://localhost:8080/hystrix.stream.
    
    There's now also an advanced mode using service discovery using Eureka which can be activated by using the cloud profile with the services. They require the eureka-server app to be running though.
    
    For more details, see the readme.

---
+++ customer-service/src/main/java/example/customers/CustomerApplication.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.customers;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.hypermedia.DiscoveredResource;
+import org.springframework.cloud.hypermedia.DynamicServiceInstanceProvider;
+import org.springframework.cloud.hypermedia.ServiceInstanceProvider;
+import org.springframework.cloud.hypermedia.StaticServiceInstanceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Spring Boot application bootstrap class to run a customer service and configure integration with a remote system that
+ * exposes a REST resource to lookup stores by location.
+ * 
+ * @author Oliver Gierke
+ */
+@SpringBootApplication
+@EnableCircuitBreaker
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	/**
+	 * A remote {@link DiscoveredResource} that provides functionality to lookup stores by location.
+	 * 
+	 * @param provider
+	 * @return
+	 */
+	@Bean
+	public DiscoveredResource storesByLocationResource(ServiceInstanceProvider provider) {
+		return new DiscoveredResource(provider, traverson -> traverson.follow("stores", "search", "by-location"));
+	}
+
+	/**
+	 * A simple default {@link ServiceInstanceProvider} to use a hard-coded remote service to detect the store locations.
+	 * 
+	 * @return
+	 */
+	@Bean
+	@Profile("default")
+	public StaticServiceInstanceProvider staticServiceInstanceProvider() {
+		return new StaticServiceInstanceProvider(new DefaultServiceInstance("stores", "localhost", 8081, false));
+	}
+
+	// Cloud configuration
+
+	/**
+	 * Dedicated configuration to rather use a {@link ServiceInstanceProvider} to lookup the remote service via a service
+	 * registry.
+	 *
+	 * @author Oliver Gierke
+	 */
+	@Profile("cloud")
+	@EnableDiscoveryClient
+	static class CloudConfiguration {
+
+		@Bean
+		public DynamicServiceInstanceProvider dynamicServiceProvider(DiscoveryClient client) {
+			return new DynamicServiceInstanceProvider(client, "stores");
+		}
+	}
+
+	// Data setup
+
+	@Autowired CustomerRepository customers;
+
+	@PostConstruct
+	public void init() {
+
+		Customer customer = new Customer("Oliver", "Gierke");
+		customer.setAddress(
+				new Address("625 Avenue of the Americas", "10011", "New York", new Location(40.740337, -73.995146)));
+
+		customers.save(customer);
+	}
+}
--- readme.md
+++ readme.md
@@ -7,3 +7,25 @@
   - Customers (JPA)
 - The customers application tries to discover a search-by-location-resource and periodically verifying it's still available (see `StoreIntegration`).
 - If the remote system is found the customers app includes a link to let clients follow to the remote system and thus find stores near the customer.
+
+## Fundamentals
+
+The core domain and focus of the example can be explored by simply starting both the customer and the store service using `mvn spring-boot:run`. The store service exposes a resource to trigger geo-spatial queries for Starbucks shops given a reference location and distance.
+
+## The customer service
+
+The customer service has a static reference to the store service configured (http://localhost:8080) and traverses a set of hypermedia links to discover the resource solely by knowing the relation names. The service then adds a link to the discovered system, expanding the link with the current location of the customer. This can be seen by follwing the `customers` relation in the root resource of the service. A `stores-nearby` link shows up.
+
+As the store system might become unavailable, we verify the presence of it by issuing a HEAD requests to the discovered resource (time interval configured by the `cloud.hypermedia.refresh.fixed-delay` property in `application.properties`).
+
+## The use of Hystrix
+
+The customer service uses Hystrix to short-circuit the discovery calls trying to find the store system if the link discovery or validation fails repeatedly. To see this working run the `hystrix-dashboard` app (`mvn spring-boot:run`), browse to http://localhost:7979/hystrix and point the dashboard to the customer service's Hystrix stream (http://localhost:8080/hystrix.stream).
+
+While the store service is running, you should see the requests being forwarded, the circuit closed. Stop the store service and see how the failing requests will trigger the circuit to be opened at some point. Restart the store service and see how after a couple of seconds the circuit gets closed again, the resource discovery is re-triggered and the `stores-nearby` link appears in the resources the customer service exposes.
+
+## Using service discovery
+
+As an alternative to the static service reference the customer service uses by default, service discovery via Eureka can be used. Make sure both the customer and store service are stopped. Start the `eureka-server` application using `mvn spring-boot:run`. Browse `http://localhost:8761` to see the Eureka web interface.
+
+Now start both the customer and the store service with the `cloud` profile enabled (`mvn spring-boot:run -Dspring.profiles.active="cloud"`). Inspecting the Eureka web interface you should see both instances being registered with the registry. The customer service now uses a `DiscoveryClient` to obtain a `ServiceInstance` by name (see the `CustomerApplication.CloudConfig.dynamicServiceProvider(…)` bean definition).
